925853ca9c41262c98f3f7f7248e6870f939a50b
Isabella Devassy <idevassy77@gmail.com>
2023-09-18 11:59:26 -0700

    completed challenges 15-17

--- example-17.js
+++ example-17.js
@@ -1,19 +1,15 @@
 // Giant Robot class
-// Goal creat an interface with a methods 
-// All Robots have name, power and type. 
-// Kaiju should have a describe method that 
-// prints: name:<name> type:<type> power:<power>
-// Timer with count
-// Goal creat a function that takes another function as an argument
-function setTimeoutCount(callback, time, count) {
-    var timer = setTimeout(function () {
-        count -= 1;
-        callback();
-        if (count === 0) {
-            clearTimeout(timer);
-        }
-    }, time);
-}
-setTimeoutCount(function () {
-    console.log('???');
-}, 1000, 3);
+var Robot = /** @class */ (function () {
+    function Robot(name, power, type) {
+        this.name = name;
+        this.power = power;
+        this.type = type;
+    }
+    Robot.prototype.describe = function () {
+        console.log("name: ".concat(this.name, " type: ").concat(this.type, " power: ").concat(this.power));
+    };
+    return Robot;
+}());
+var robot = new Robot('Robo', 100, 'robot');
+robot.describe();
+exports.Robot = Robot;
--- example-17.ts
+++ example-17.ts
@@ -6,4 +6,29 @@
 // Kaiju should have a describe method that 
 // prints: name:<name> type:<type> power:<power>
 
+interface RobotInterface {
+    name: string;
+    power: number;
+    type: string;
+}
 
+class Robot implements RobotInterface {
+    name: string;
+    power: number;
+    type: string;
+
+    constructor(name: string, power: number, type: string) {
+        this.name = name;
+        this.power = power;
+        this.type = type;
+    }
+
+    describe(): void {
+        console.log(`name: ${this.name} type: ${this.type} power: ${this.power}`)
+    }
+}
+
+const robot = new Robot('Robo', 100, 'robot');
+robot.describe();
+
+exports.Robot = Robot;
